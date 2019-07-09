b4b0d76d859396fc537d3282ddd4bae21a448bba
Diego Hernan Aguilar Gomez <dhag100993@hotmail.com>
2019-07-09 10:27:32 -0500

    pruebas

---
+++ Plataforma_academica/Plataforma_academica/Controllers/AcademicaProfesionalController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plataforma_academica.Controllers
+{
+    public class AcademicaProfesionalController : Controller
+    {
+        // GET: AcademicaProfesional
+        public ActionResult Academicaprofesional()
+        {
+            String codig = Request.Form["ir"];
+
+            if (codig == "profesional")
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Academicainfantil", "AcademicaProfesional");
+            }
+        }
+
+        public ActionResult Academicainfantil()
+        {
+            
+            return View();
+        }
+
+        public ActionResult login()
+        {
+
+            return RedirectToAction("principalplataforma", "PrincipalPlataforma");
+        }
+    }
+}
