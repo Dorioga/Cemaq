cc7d3135fa6c54e2d880a36b0d2834bf94bb8864
alejandro <alejozuleta9217@gmail.com>
2019-07-12 10:13:33 -0500

    primer cambio
    se realiaza la interfaz actividad

---
+++ Plataforma_academica/Plataforma_academica/Controllers/ActividadesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plataforma_academica.Controllers
+{
+    public class ActividadesController : Controller
+    {
+        // GET: Actividades
+        public ActionResult Actividades()
+        {
+            return View();
+        }
+    }
+}
