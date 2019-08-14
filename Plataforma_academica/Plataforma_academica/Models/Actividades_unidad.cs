a1827281575ad5ac8f7b287db4aed38a2923da37
Diego Hernan Aguilar Gomez <dhag100993@hotmail.com>
2019-08-14 09:18:27 -0500

    Actualizacion General 14/08/2019
    Actualizacion de 2 usuarios

--- Plataforma_academica/Plataforma_academica/Models/Actividades_unidad.cs
+++ Plataforma_academica/Plataforma_academica/Models/Actividades_unidad.cs
@@ -10,22 +10,35 @@ namespace Plataforma_academica.Models
     {
         public String nombre_actividad { set; get; }
         public String codigo_actividad { set; get; }
+        public String edicion { set; get; }
 
         public Actividades_unidad[] Consultar_Actividad_unidades(string nc)
         {
+            //@ -27,5 + 28,23 @@ namespace Plataforma_academica.Models
+            
             Conexion.Conexion con = new Conexion.Conexion();
-            DataTable porcentaje = con.Execute_Query("call Pr_cargar_actividad(" + nc + ")");
+            DataTable porcentaje = con.Execute_Query("call Pr_cargar_actividad_edic(" + nc + ")");
             Actividades_unidad[] arreglo = new Actividades_unidad[porcentaje.Rows.Count];
-            int j = 0;
-            foreach (DataRow i in porcentaje.Rows)
-            {
-                arreglo[j] = new Actividades_unidad();
-                arreglo[j].nombre_actividad = i["nombre actividad"].ToString();
-                arreglo[j].codigo_actividad = i["codigo actividad"].ToString();
-                j++;
-            }
             return arreglo;
 
         }
+
+        public Actividades_unidad[] Consultar_Actividad_unidades_edic(string nc)
+    {
+        Conexion.Conexion con = new Conexion.Conexion();
+        DataTable porcentaje = con.Execute_Query("call Pr_cargar_actividad_edic(" + nc + ")");
+        Actividades_unidad[] arreglo = new Actividades_unidad[porcentaje.Rows.Count];
+        int j = 0;
+        foreach (DataRow i in porcentaje.Rows)
+        {
+            arreglo[j] = new Actividades_unidad();
+            arreglo[j].nombre_actividad = i["nombre actividad"].ToString();
+            arreglo[j].codigo_actividad = i["codigo actividad"].ToString();
+            arreglo[j].edicion = i["Editado"].ToString();
+            j++;
+        }
+        return arreglo;
+
     }
+}
 }
