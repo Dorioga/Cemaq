2783deda8ba17fbe98f039232e8544c65a63f0ea
Diego Hernan Aguilar Gomez <dhag100993@hotmail.com>
2019-08-14 08:18:17 -0500

    Modificacion 14/08/2019
    Modificacion Interfaz inicio y reestructuracion del Codigo

--- Plataforma_academica/Plataforma_academica/Models/Actividades_unidad.cs
+++ Plataforma_academica/Plataforma_academica/Models/Actividades_unidad.cs
@@ -10,7 +10,6 @@ namespace Plataforma_academica.Models
     {
         public String nombre_actividad { set; get; }
         public String codigo_actividad { set; get; }
-        public String edicion { set; get; }
 
         public Actividades_unidad[] Consultar_Actividad_unidades(string nc)
         {
@@ -23,24 +22,6 @@ namespace Plataforma_academica.Models
                 arreglo[j] = new Actividades_unidad();
                 arreglo[j].nombre_actividad = i["nombre actividad"].ToString();
                 arreglo[j].codigo_actividad = i["codigo actividad"].ToString();
-                j++;
-            }
-            return arreglo;
-
-        }
-
-        public Actividades_unidad[] Consultar_Actividad_unidades_edic(string nc)
-        {
-            Conexion.Conexion con = new Conexion.Conexion();
-            DataTable porcentaje = con.Execute_Query("call Pr_cargar_actividad_edic(" + nc + ")");
-            Actividades_unidad[] arreglo = new Actividades_unidad[porcentaje.Rows.Count];
-            int j = 0;
-            foreach (DataRow i in porcentaje.Rows)
-            {
-                arreglo[j] = new Actividades_unidad();
-                arreglo[j].nombre_actividad = i["nombre actividad"].ToString();
-                arreglo[j].codigo_actividad = i["codigo actividad"].ToString();
-                arreglo[j].edicion = i["Editado"].ToString();
                 j++;
             }
             return arreglo;
