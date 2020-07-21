62c8fdc7903959876feb4d5caa315af7833bc913
Diego Hernan Aguilar Gomez <dhag100993@hotmail.com>
2020-07-21 08:55:36 -0500

    Modo oscuro para principal plataforma y la pagina maestra

--- Plataforma_academica/Plataforma_academica/Scripts/Modooscuro.js
+++ Plataforma_academica/Plataforma_academica/Scripts/Modooscuro.js
@@ -1,6 +1,26 @@
-﻿function cambiarmodo() {
-    var enca = document.getElementById("head");
-    var menizq = document.getElementsByClassName("exterior");
-    enca.classList.toggle("oscuro");
-    menizq.toggle("oscuro");
-}
+﻿const btnswitch = document.querySelector('#switch');
+
+btnswitch.addEventListener('click', function(){
+    //Agregar/quitar Modo oscuro 
+    document.body.classList.toggle('dark');
+    btnswitch.classList.toggle('active');
+
+    //Guardamos el modo en localstorage
+        //Preguntar si tiene la clase Dark
+    if (document.body.classList.contains('dark')) {
+        //Se guarda en Local Storage la configuracion con nombre Darkmode
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+   
+    //Obtener El modo actual
+    if (localStorage.getItem('dark-mode') === 'true') {
+        document.body.classList.add('dark');
+        btnswitch.classList.add('active');
+    } else {
+        document.body.classList.remove('dark');
+        btnswitch.classList.remove('active');
+    }
+
