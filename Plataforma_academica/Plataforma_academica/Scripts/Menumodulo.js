35ba3cfdffe2d2931ba759d046d5d49c4e4652f2
Diego Hernan Aguilar Gomez <dhag100993@hotmail.com>
2021-02-12 15:00:59 -0500

    Cambios Mene izq

---
+++ Plataforma_academica/Plataforma_academica/Scripts/Menumodulo.js
@@ -0,0 +1,18 @@
+﻿/*var li = document.getElementsByClassName("titulo");
+var li2;
+
+var cod = 0;
+for (var i = 0; i < li.length; i++){
+  //var a =  li.childNodes[i].nodeValue;
+    li[i].addEventListener('click', mostrar(li[i].), false);
+}*/
+var mod = document.getElementsByClassName("modulo");
+function mostrarmodulo(value_elemento) {
+    var cod = value_elemento;
+    
+    for (var i = 0; i < mod.length; i++) {
+        if (cod == mod[i].value) {
+            mod[i].classList.toggle('active');
+        }
+    }
+}
