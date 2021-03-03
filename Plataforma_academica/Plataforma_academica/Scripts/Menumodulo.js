78fab0a4a5f4ede4bb0c03a6b2c1b713375df7eb
Diego Hernan Aguilar Gomez <dhag100993@hotmail.com>
2021-03-03 14:42:11 -0500

    cambios Menu Izq

--- Plataforma_academica/Plataforma_academica/Scripts/Menumodulo.js
+++ Plataforma_academica/Plataforma_academica/Scripts/Menumodulo.js
@@ -1,18 +1,34 @@
-﻿/*var li = document.getElementsByClassName("titulo");
-var li2;
-
-var cod = 0;
-for (var i = 0; i < li.length; i++){
-  //var a =  li.childNodes[i].nodeValue;
-    li[i].addEventListener('click', mostrar(li[i].), false);
-}*/
+﻿
 var mod = document.getElementsByClassName("modulo");
-function mostrarmodulo(value_elemento) {
+
+
+function mostrarmodulo(value_elemento, eleccion) {
+    console.log("la eleccion es : " + eleccion);
     var cod = value_elemento;
-    
+    var ele = eleccion;
     for (var i = 0; i < mod.length; i++) {
         if (cod == mod[i].value) {
-            mod[i].classList.toggle('active');
+            if (ele == 'titulo') {
+                mod[i].classList.toggle('active');
+            } else {
+                if (ele == 'modulo') {
+                    mod[i].classList.add('active');
+                }
+            }            
+            localStorage.setItem('valor', mod[i].value);
+        } else {
+            mod[i].classList.remove('active');
         }
     }
 }
+
+//Cargar la Funcion Guardada
+var aux = localStorage.getItem('valor');
+
+    for (var j = 0; j < mod.length; j++) {
+        if (mod[j].value == aux) {
+            if (!mod[j].classList.contains('active')) {
+                mod[j].classList.add('active');
+            }
+        }
+    }
