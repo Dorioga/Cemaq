f1bec7c5c81f77ae29eb51d3857b9f4443c1287d
Alejo1117525453# <alejozuleta9217@gmail.com>
2021-08-28 08:18:13 -0500

    publicacion y registros

--- Plataforma_academica/Plataforma_academica/obj/Release/Package/PackageTmp/Scripts/procentaje.js
+++ Plataforma_academica/Plataforma_academica/obj/Release/Package/PackageTmp/Scripts/procentaje.js
@@ -7,7 +7,7 @@
      if (Convert.ToInt32(actividad.datos1.Rows[0]["porcentaje"].ToString()) < 100)
      {
          actividad.datos = us.Buscar_cantidad_secciones(actividad1.codigo_actividad);
-         for (int j = 1; j <= Convert.ToInt32(actividad.datos.Rows[0]["numero secciones"].ToString()); j++)
+         for (var j = 1; j <= Convert.ToInt32(actividad.datos.Rows[0]["numero secciones"].ToString()); j++)
          {
 
          }
